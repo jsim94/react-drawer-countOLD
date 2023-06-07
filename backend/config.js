acf1c94cc3e35ef654ecd4351c801f70efcf1979
James Sime <j.sime94@gmail.com>
2023-06-07 05:06:19 -0400

    added DB_URI env variable

--- backend/config.js
+++ backend/config.js
@@ -10,9 +10,10 @@ const PORT = +process.env.PORT || 3001;
 // Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
   function genUri() {
-    const { DB_USER, DB_PW } = process.env;
+    const { DB_USER, DB_PW, DB_URI } = process.env;
     const dbName =
-      process.env.NODE_ENV === "test" ? "drawer_count_test" : "drawer_count";
+      DB_URI ||
+      (process.env.NODE_ENV === "test" ? "drawer_count_test" : "drawer_count");
     return `postgresql://${DB_USER}:${DB_PW}@localhost/${dbName}`;
   }
 
