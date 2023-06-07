67965c6bc912c59a9e5b75177472c7d54d02540a
James Sime <j.sime94@gmail.com>
2023-06-07 13:06:39 -0400

    changed health endpoint.  debugging sql cmd

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/history", historyRoutes);
 app.use("/api/users", userRoutes);
 
-app.get("/api", async (req, res, next) => {
+app.get("/", async (req, res, next) => {
   return res.status(200).json({ status: "ok" });
 });
 
